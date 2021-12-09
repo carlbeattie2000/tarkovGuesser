6c617151a243e1e5a19e6db4d73a17042a3d408f
carl beattie <12beattiecastp@gmail.com>
2021-12-09 23:54:44 +0000

    feat: loading diffrent maps| bugFix: zoom glass was duping

--- assets/js/globalFunctions.js
+++ assets/js/globalFunctions.js
@@ -1,3 +1,23 @@
+let magnifyGlassInit = false;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
+
+// document.getElementById("mainImage").addEventListener("mouseover", () => {
+//     if (!magnifyGlassInit) {
+//         magnify("mainImage", 2);
+//         magnifyGlassInit = true;
+//     }
+// });
+
+document.getElementById("start-game").addEventListener("click", () => {
+    let glass = document.getElementById("maginfyGlass");
+
+    if (glass) {
+        glass.parentNode.removeChild(glass);
+    }
+
+    customsStart();
+    magnify("mainImage", 2);
+});
