8a562c24276c42afdfbcda5c487000bca10979f0
carlbeattie2000 <12beattiecastp@gmail.com>
2021-12-10 04:36:47 +0000

    fixed: map tiles | Added: 3 maps

--- assets/js/globalFunctions.js
+++ assets/js/globalFunctions.js
@@ -1,11 +1,25 @@
-let magnifyGlassInit = false;
+var map = L.map('map').setView([0, 0], 2);
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+const setMapTiles = (tilesPath, maxZoom) => {
+    L.tileLayer(`./assets/maps/${tilesPath}/{z}/{x}/{y}.png`, {
+        minZoom: 1,
+        maxZoom: maxZoom,
+        tileSize: 256, // or 512  
+        crs: L.CRS.Simple, // this is important for nongeo maps
+        attribution: 'ESO/INAF-VST/OmegaCAM',
+        tms: true,
+        noWrap: true
+    }).addTo(map);
 }
+
+// set default map to customs
+setMapTiles("customTiles", 6);
+
+// handle map changing
 
 document.getElementById("start-game").addEventListener("click", () => {
     let glass = document.getElementById("maginfyGlass");
+    let mapSelectValue = document.getElementById("map-select").value;
 
     if (glass) {
         glass.parentNode.removeChild(glass);
@@ -14,3 +28,7 @@ document.getElementById("start-game").addEventListener("click", () => {
     customsStart();
     magnify("mainImage", 2);
 });
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
