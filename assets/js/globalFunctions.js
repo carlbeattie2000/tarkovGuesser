b8479d9b925ee941cb799646a9057cf51076309b
carlbeattie2000 <12beattiecastp@gmail.com>
2021-12-10 04:50:17 +0000

    feat: change maps

--- assets/js/globalFunctions.js
+++ assets/js/globalFunctions.js
@@ -1,6 +1,11 @@
-var map = L.map('map').setView([0, 0], 2);
+let map = L.map('map').setView([0, 0], 2);
 
 const setMapTiles = (tilesPath, maxZoom) => {
+    map.off();
+    map.remove();
+
+    map = L.map('map').setView([0, 0], 2);
+
     L.tileLayer(`./assets/maps/${tilesPath}/{z}/{x}/{y}.png`, {
         minZoom: 1,
         maxZoom: maxZoom,
@@ -21,11 +26,29 @@ document.getElementById("start-game").addEventListener("click", () => {
     let glass = document.getElementById("maginfyGlass");
     let mapSelectValue = document.getElementById("map-select").value;
 
+    switch(mapSelectValue) {
+        case "customs" :
+            setMapTiles("customTiles", 6);
+            customsStart();
+            break
+        case "factory" :
+            setMapTiles("factoryTiles", 2);
+            break
+        case "reserve" :
+            setMapTiles("reserveTiles", 4);
+            break
+        case "interchange" :
+            setMapTiles("interchangeTiles", 3);
+            break
+        default :
+            console.log("error");
+            break
+    }
+
     if (glass) {
         glass.parentNode.removeChild(glass);
     }
 
-    customsStart();
     magnify("mainImage", 2);
 });
 
