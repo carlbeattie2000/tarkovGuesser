4255987b34274d8e74eb5079e7af02b422d90d40
carl beattie <12beattiecastp@gmail.com>
2021-12-10 00:05:14 +0000

    add: github source link

--- assets/js/globalFunctions.js
+++ assets/js/globalFunctions.js
@@ -4,13 +4,6 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-// document.getElementById("mainImage").addEventListener("mouseover", () => {
-//     if (!magnifyGlassInit) {
-//         magnify("mainImage", 2);
-//         magnifyGlassInit = true;
-//     }
-// });
-
 document.getElementById("start-game").addEventListener("click", () => {
     let glass = document.getElementById("maginfyGlass");
 
